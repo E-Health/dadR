716cc414306c0709cfc80fbc9acd2a101d446782
Bell Eapen <github_public@gulfdoctor.net>
2018-05-26 15:16:25 -0400

    SparkSameDisease refactoring.

---
+++ R/DADSameDisease.R
@@ -0,0 +1,46 @@
+#' Title DADSameDisease - List all diseases with given diagnosis.
+#'
+#' @param disease
+#' @param spark_data_frame
+#'
+#' @return spark_data_frame with records 
+#' @export
+#'
+#' @examples
+#' spark_df <- DADSameDisease("J08")
+#'   r_df <- collect(spark_df)
+#'   r_dt <- as.data.table(r_df)
+#'   r_dt  # All records with the diagnosis J08
+DADSameDisease <- function(disease, spark_data_frame = NULL){
+    if(!is.null(spark_data_frame)){
+        createOrReplaceTempView(spark_data_frame, "spark_data_frame")
+    }
+    # sql_query = "SELECT * FROM spark_data_frame WHERE D_I10_1 = 'J09'"
+    sql_query = paste("SELECT * FROM spark_data_frame WHERE",
+    " D_I10_1 = '", disease, "' OR ",
+    " D_I10_2 = '", disease, "' OR ",
+    " D_I10_3 = '", disease, "' OR ",
+    " D_I10_4 = '", disease, "' OR ",
+    " D_I10_5 = '", disease, "' OR ",
+    " D_I10_6 = '", disease, "' OR ",
+    " D_I10_7 = '", disease, "' OR ",
+    " D_I10_8 = '", disease, "' OR ",
+    " D_I10_9 = '", disease, "' OR ",
+    " D_I10_10 = '", disease, "' OR ",
+    " D_I10_11 = '", disease, "' OR ",
+    " D_I10_12 = '", disease, "' OR ",
+    " D_I10_13 = '", disease, "' OR ",
+    " D_I10_14 = '", disease, "' OR ",
+    " D_I10_15 = '", disease, "' OR ",
+    " D_I10_16 = '", disease, "' OR ",
+    " D_I10_17 = '", disease, "' OR ",
+    " D_I10_18 = '", disease, "' OR ",
+    " D_I10_19 = '", disease, "' OR ",
+    " D_I10_20 = '", disease, "' OR ",
+    " D_I10_21 = '", disease, "' OR ",
+    " D_I10_22 = '", disease, "' OR ",
+    " D_I10_23 = '", disease, "' OR ",
+    " D_I10_24 = '", disease, "' OR ",
+    " D_I10_25 = '", disease, "'", sep="")
+    return(sql(sql_query))
+}
