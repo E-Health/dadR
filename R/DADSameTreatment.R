ed022201a3ad584b6cda8edc35f1ce341cedb1f4
Bellraj Eapen <github@gulfdoctor.net>
2018-06-02 19:05:38 -0400

    DADSameTreatment test added and = changed to LIKE

--- R/DADSameTreatment.R
+++ R/DADSameTreatment.R
@@ -3,7 +3,7 @@
 #' @param intervention
 #' @param spark_data_frame
 #'
-#' @return spark_data_frame with records 
+#' @return spark_data_frame with records
 #' @export
 #'
 #' @examples
@@ -16,25 +16,25 @@ DADSameTreatment <- function(treatment, spark_data_frame = NULL){
         createOrReplaceTempView(spark_data_frame, "spark_data_frame")
     }
     sql_query = paste("SELECT * FROM spark_data_frame WHERE",
-    " I_CCI_1 = '", treatment, "' OR ",
-    " I_CCI_2 = '", treatment, "' OR ",
-    " I_CCI_3 = '", treatment, "' OR ",
-    " I_CCI_4 = '", treatment, "' OR ",
-    " I_CCI_5 = '", treatment, "' OR ",
-    " I_CCI_6 = '", treatment, "' OR ",
-    " I_CCI_7 = '", treatment, "' OR ",
-    " I_CCI_8 = '", treatment, "' OR ",
-    " I_CCI_9 = '", treatment, "' OR ",
-    " I_CCI_10 = '", treatment, "' OR ",
-    " I_CCI_11 = '", treatment, "' OR ",
-    " I_CCI_12 = '", treatment, "' OR ",
-    " I_CCI_13 = '", treatment, "' OR ",
-    " I_CCI_14 = '", treatment, "' OR ",
-    " I_CCI_15 = '", treatment, "' OR ",
-    " I_CCI_16 = '", treatment, "' OR ",
-    " I_CCI_17 = '", treatment, "' OR ",
-    " I_CCI_18 = '", treatment, "' OR ",
-    " I_CCI_19 = '", treatment, "' OR ",
-    " I_CCI_20 = '", treatment, "'", sep="")
+                      " I_CCI_1 LIKE '", treatment, "%' OR ",
+                      " I_CCI_2 LIKE '", treatment, "%' OR ",
+                      " I_CCI_3 LIKE '", treatment, "%' OR ",
+                      " I_CCI_4 LIKE '", treatment, "%' OR ",
+                      " I_CCI_5 LIKE '", treatment, "%' OR ",
+                      " I_CCI_6 LIKE '", treatment, "%' OR ",
+                      " I_CCI_7 LIKE '", treatment, "%' OR ",
+                      " I_CCI_8 LIKE '", treatment, "%' OR ",
+                      " I_CCI_9 LIKE '", treatment, "%' OR ",
+                      " I_CCI_10 LIKE '", treatment, "%' OR ",
+                      " I_CCI_11 LIKE '", treatment, "%' OR ",
+                      " I_CCI_12 LIKE '", treatment, "%' OR ",
+                      " I_CCI_13 LIKE '", treatment, "%' OR ",
+                      " I_CCI_14 LIKE '", treatment, "%' OR ",
+                      " I_CCI_15 LIKE '", treatment, "%' OR ",
+                      " I_CCI_16 LIKE '", treatment, "%' OR ",
+                      " I_CCI_17 LIKE '", treatment, "%' OR ",
+                      " I_CCI_18 LIKE '", treatment, "%' OR ",
+                      " I_CCI_19 LIKE '", treatment, "%' OR ",
+                      " I_CCI_20 LIKE '", treatment, "%'", sep="")
     return(sql(sql_query))
 }
---
+++ tests/testthat/test_DadSameTreatment.R
@@ -0,0 +1,24 @@
+context("Same Treatment")
+
+tmp <- tempfile()
+
+setup({
+
+  # writeLines("TEST DATA", tmp)
+
+})
+teardown({
+
+  print( readLines(tmp) )
+  unlink(tmp)
+
+})
+
+
+test_that('returns patients with same treatment', {
+
+  spark_df <- DADSameTreatment("I.")
+  r_df <- collect(spark_df)
+  r_dt <- as.data.table(r_df)
+  expect_known_output(r_dt, tmp, print = TRUE)
+})
