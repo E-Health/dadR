20542566f2b44e7eea304679f0ce1888a336cbb5
Bellraj Eapen <github@gulfdoctor.net>
2018-06-02 19:25:52 -0400

    Comorbidity added.

--- R/DADComorbidity.R
+++ R/DADComorbidity.R
@@ -14,30 +14,25 @@ DADComorbidity <- function(disease, spark_data_frame = NULL){
 
   patient_set = DADSameDisease(disease)
   createOrReplaceTempView(patient_set, "patient_set")
-  comorbidity = sql("SELECT D_I10_1 as disease FROM patient_set")
-  comorbidity = unionAll(comorbidity, sql("SELECT D_I10_2 as disease FROM patient_set"))
-  comorbidity = unionAll(comorbidity, sql("SELECT D_I10_3 as disease FROM patient_set"))
-  comorbidity = unionAll(comorbidity, sql("SELECT D_I10_4 as disease FROM patient_set"))
-  comorbidity = unionAll(comorbidity, sql("SELECT D_I10_5 as disease FROM patient_set"))
-  comorbidity = unionAll(comorbidity, sql("SELECT D_I10_6 as disease FROM patient_set"))
-  comorbidity = unionAll(comorbidity, sql("SELECT D_I10_7 as disease FROM patient_set"))
-  comorbidity = unionAll(comorbidity, sql("SELECT D_I10_8 as disease FROM patient_set"))
-  comorbidity = unionAll(comorbidity, sql("SELECT D_I10_9 as disease FROM patient_set"))
-  comorbidity = unionAll(comorbidity, sql("SELECT D_I10_10 as disease FROM patient_set"))
-  comorbidity = unionAll(comorbidity, sql("SELECT D_I10_11 as disease FROM patient_set"))
-  comorbidity = unionAll(comorbidity, sql("SELECT D_I10_12 as disease FROM patient_set"))
-  comorbidity = unionAll(comorbidity, sql("SELECT D_I10_13 as disease FROM patient_set"))
-  comorbidity = unionAll(comorbidity, sql("SELECT D_I10_14 as disease FROM patient_set"))
-  comorbidity = unionAll(comorbidity, sql("SELECT D_I10_15 as disease FROM patient_set"))
-  comorbidity = unionAll(comorbidity, sql("SELECT D_I10_16 as disease FROM patient_set"))
-  comorbidity = unionAll(comorbidity, sql("SELECT D_I10_17 as disease FROM patient_set"))
-  comorbidity = unionAll(comorbidity, sql("SELECT D_I10_18 as disease FROM patient_set"))
-  comorbidity = unionAll(comorbidity, sql("SELECT D_I10_19 as disease FROM patient_set"))
-  comorbidity = unionAll(comorbidity, sql("SELECT D_I10_20 as disease FROM patient_set"))
-  comorbidity = unionAll(comorbidity, sql("SELECT D_I10_21 as disease FROM patient_set"))
-  comorbidity = unionAll(comorbidity, sql("SELECT D_I10_22 as disease FROM patient_set"))
-  comorbidity = unionAll(comorbidity, sql("SELECT D_I10_23 as disease FROM patient_set"))
-  comorbidity = unionAll(comorbidity, sql("SELECT D_I10_24 as disease FROM patient_set"))
-  comorbidity = unionAll(comorbidity, sql("SELECT D_I10_25 as disease FROM patient_set"))
-  return(summarize(groupBy(comorbidity, comorbidity$disease), count = n(comorbidity$disease)))
+  comorbidity = sql("SELECT
+  CONCAT(D_I10_1, ' ', D_I10_2,
+  ' ', D_I10_3,
+  ' ', D_I10_4,
+  ' ', D_I10_5,
+  ' ', D_I10_6,
+  ' ', D_I10_7,
+  ' ', D_I10_8,
+  ' ', D_I10_9,
+  ' ', D_I10_10,
+  ' ', D_I10_11,
+  ' ', D_I10_12,
+  ' ', D_I10_13,
+  ' ', D_I10_14,
+  ' ', D_I10_15,
+  ' ', D_I10_16,
+  ' ', D_I10_17,
+  ' ', D_I10_18,
+  ' ', D_I10_19,
+  ' ', D_I10_20) as disease FROM patient_set")
+   return(summarize(groupBy(comorbidity, comorbidity$disease), count = n(comorbidity$disease)))
 }
