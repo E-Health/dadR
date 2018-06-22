db5b82c5e558799a03d5f9474ea7ccc820fa79dd
dermatologist <github@gulfdoctor.net>
2018-06-22 08:24:46 -0400

    helper.R updated to read spark masterUI from file.

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -4,8 +4,25 @@ library(SparkR)
 library(data.table)
 library(foreign)
 
-# Change Master UI here
-sparkR.session(master = "spark://cdr541.int.cedar.computecanada.ca:7077", sparkConfig = list(spark.driver.memory = "3g", spark.executor.memory = "3g"))
+# Try to read the master UI from csvFile
+# https://stackoverflow.com/questions/12626637/reading-a-text-file-in-r-line-by-line
+    con = file("/home/beapen/scratch/dadr.out", "r")
+    masterui = "spark://cdr541.int.cedar.computecanada.ca:7077"
+    while ( TRUE ) {
+        line = readLines(con, n = 1)
+        if ( length(line) == 0 ) {
+            break
+        }
+        # print(line)
+        if (grepl("spark://cdr", line)){
+            masterui = line
+        }
+    }
 
-DADSparkInit(csvFile = "/home/beapen/scratch/dadr.csv")
+    close(con)
+
+# Change Master UI here
+sparkR.session(master = masterui, sparkConfig = list(spark.driver.memory = "3g", spark.executor.memory = "3g"))
+
+DADSparkInit(csvFile = "/home/beapen/project/beapen/dadr/dadr.csv")
 
